7c89bdaa6b2a2e310a20a44a5c511be6bcdbd0e4
kampffrosch94 <kampffrosch94@github.com>
2025-08-23 21:31:23 +0200

    fix static build

--- app/src/static_worker.rs
+++ app/src/static_worker.rs
@@ -1,18 +1,17 @@
 use base::ContextTrait;
+use worker::PersistentState;
 use worker::{self, update_inner};
-use worker::{FleetingState, PersistentState};
 
 pub struct StaticWorker {
-    fleeting: FleetingState,
     persistent: PersistentState,
 }
 
 impl StaticWorker {
     pub fn new() -> Self {
-        StaticWorker { fleeting: FleetingState::new(), persistent: PersistentState::new() }
+        StaticWorker { persistent: PersistentState::new() }
     }
 
     pub fn update(&mut self, c: &mut dyn ContextTrait) {
-        update_inner(c, &mut self.persistent, &mut self.fleeting);
+        update_inner(c, &mut self.persistent);
     }
 }
