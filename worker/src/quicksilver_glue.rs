55308db1767dabe370190e2aff22dbab1530a038
kampffrosch94 <kampffrosch94@github.com>
2025-11-01 20:47:14 +0100

    entitywrapper as prep for ser/de of entity with generation

--- worker/src/quicksilver_glue.rs
+++ worker/src/quicksilver_glue.rs
@@ -1,40 +1,56 @@
-use froql::entity_store::{Entity, EntityGeneration, EntityId};
-use quicksilver::Quicksilver;
+use froql::entity_store::Entity;
 
-#[expect(unused)]
-pub const ENITIY_MIRROR: ::quicksilver::Type =
-    ::quicksilver::Type::Struct(&::quicksilver::Struct {
-        name: "Entity",
-        size: ::std::mem::size_of::<Entity>(),
-        align: align_of::<Entity>(),
+#[derive(Debug, Clone, Copy)]
+#[repr(transparent)]
+pub struct EntityWrapper(Entity);
+
+impl ::quicksilver::Quicksilver for EntityWrapper {
+    const MIRROR: ::quicksilver::Type = ::quicksilver::Type::Struct(&::quicksilver::Struct {
+        name: "EntityWrapper",
+        size: ::std::mem::size_of::<Self>(),
+        align: align_of::<Self>(),
         fields: &[
             ::quicksilver::Field {
-                name: "generation",
-                ty: ::quicksilver::Type::Struct(&::quicksilver::Struct {
-                    name: "EntityGeneration",
-                    size: ::std::mem::size_of::<EntityGeneration>(),
-                    align: align_of::<EntityGeneration>(),
-                    fields: &[::quicksilver::Field {
-                        name: "0",
-                        ty: u32::MIRROR,
-                        offset: ::std::mem::offset_of!(EntityGeneration, 0),
-                    }],
-                }),
-                offset: ::std::mem::offset_of!(Entity, generation),
+                name: "id",
+                ty: u32::MIRROR,
+                offset: ::std::mem::offset_of!(Self, 0.id),
             },
             ::quicksilver::Field {
-                name: "id",
-                ty: ::quicksilver::Type::Struct(&::quicksilver::Struct {
-                    name: "EntityId",
-                    size: std::mem::size_of::<EntityId>(),
-                    align: align_of::<EntityId>(),
-                    fields: &[::quicksilver::Field {
-                        name: "0",
-                        ty: u32::MIRROR,
-                        offset: ::std::mem::offset_of!(EntityId, 0),
-                    }],
-                }),
-                offset: ::std::mem::offset_of!(Entity, id),
+                name: "gen",
+                ty: u32::MIRROR,
+                offset: ::std::mem::offset_of!(Self, 0.generation),
             },
         ],
     });
+}
+
+impl From<Entity> for EntityWrapper {
+    fn from(value: Entity) -> Self {
+        Self(value)
+    }
+}
+
+const _: () = const {
+    // check that there are only id and gen fields in entity
+    assert!(size_of::<EntityWrapper>() == 2 * size_of::<u32>());
+};
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use froql::world::World;
+    use quicksilver::{json::from_json, reflections_ref::reflect_ref};
+
+    #[test]
+    fn test_roundtrip() {
+        let mut world = World::new();
+        let e = EntityWrapper(world.create_entity());
+        let s = reflect_ref(&e).to_json();
+        println!("{s}");
+        let e2: EntityWrapper = from_json(&s);
+        assert_eq!(e.0, e2.0)
+    }
+
+    #[test]
+    fn test_size_assumption() {}
+}
