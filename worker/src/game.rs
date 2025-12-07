11c40353dfbdd179710c873075610e85b63b054f
kampffrosch94 <kampffrosch94@github.com>
2025-12-07 17:34:46 +0100

    move game_ai into own file

--- worker/src/game.rs
+++ worker/src/game.rs
@@ -8,6 +8,7 @@ use base::{Color, ContextTrait, FPos, Input, Pos, Rect, grids::Grid, shadowcasti
 use base::{FVec, TextProperty};
 use debug_util::{DebugOptions, debug_ui};
 use froql::{entity_store::Entity, query, world::World};
+use game_ai::ai_turn;
 use input_handling::{avy_navigation, input_direction, player_inputs};
 use mapgen::{generate_map, place_enemies};
 use quicksilver::Quicksilver;
@@ -15,6 +16,7 @@ use sprites::CreatureSprite;
 use std::collections::HashSet;
 use tile_map::{DecorWithPos, TileMap};
 pub mod ecs_types;
+pub mod game_ai;
 pub mod input_handling;
 pub mod sprites;
 pub mod tile_map;
@@ -247,69 +249,6 @@ fn handle_action(world: &World, action: Action) {
             handle_death(world, target, &target_a, animation);
         }
     };
-}
-
-// TODO return action instead of handling it directly
-fn ai_turn(world: &mut World, npc: Entity) {
-    zone!();
-    {
-        let mut actor = world.get_component_mut::<Actor>(npc);
-        if actor.hp.current <= 0 {
-            // dead actors wait for further handling
-            // might be better to handle it with a death marker component or something
-            // that could then exclude actors from the turn order
-            actor.next_turn += 100;
-            return;
-        }
-    }
-
-    // set up pathfinding dijsktra map
-    let start = world.get_component::<Actor>(npc).pos;
-    let tm = world.singleton::<TileMap>();
-    let grid = {
-        let mut grid = Grid::new(tm.tiles.width, tm.tiles.height, 0);
-        let mut seeds = Vec::new();
-        for (player,) in query!(world, Actor, _ Player) {
-            grid[player.pos] = 500;
-            seeds.push(player.pos);
-        }
-        let cost_function = |pos| {
-            if tm.is_wall(pos) && pos != start {
-                i32::MAX
-            } else if tm.get_actor(pos).is_some() {
-                25
-            } else {
-                1
-            }
-        };
-        dijkstra(&mut grid, &seeds, cost_function);
-        grid
-    };
-
-    // do pathfinding
-    let start = world.get_component::<Actor>(npc).pos;
-    let path = dijkstra_path(&grid, start);
-
-    if path.len() > 1
-        && let Some(next) = path[1..].first()
-        && !tm.is_blocked(*next)
-    {
-        let action = Action { actor: npc, kind: ActionKind::Move { from: start, to: *next } };
-        handle_action(world, action);
-    } else if path.len() > 1
-        && let Some(next) = path[1..].first()
-        && let Some(target) = tm.actors.get(next)
-        && world.has_component::<Player>(*target)
-    {
-        // attack player if the player is the thing blocking movement
-        let action = Action { actor: npc, kind: ActionKind::BumpAttack { target: *target } };
-        handle_action(world, action);
-    } else {
-        // just stand in place
-        // TODO add wait action
-        let mut actor = world.get_component_mut::<Actor>(npc);
-        actor.next_turn += 10;
-    }
 }
 
 fn next_turn_actor(world: &World) -> Entity {
